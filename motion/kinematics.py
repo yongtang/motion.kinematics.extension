17535ce1a3bd849ee7a35aebefc8f959601f805d
Yong Tang <yong.tang.github@outlook.com>
2025-03-17 04:17:45 +0000

    Motion Kinematics Extension

--- motion/kinematics.py
+++ motion/kinematics.py
@@ -10,12 +10,11 @@ import numpy as np
 import PIL.Image
 
 
-class MotionExtension(omni.ext.IExt):
+class MotionKinematicsExtension(omni.ext.IExt):
     def __init__(self):
         super().__init__()
 
         self.config = {
-            "camera": None,
             "effector": None,
             "articulation": None,
             "server": "ws://localhost:8081",
@@ -26,12 +25,9 @@ class MotionExtension(omni.ext.IExt):
             ext_id = ext_manager.get_extension_id_by_module(__name__)
             ext_path = ext_manager.get_extension_path(ext_id)
             config = os.path.join(ext_path, "config", "config.toml")
-            print("[MotionExtension] Extension config: {}".format(config))
+            print("[MotionKinematicsExtension] Extension config: {}".format(config))
             config = toml.load(config)
-            print("[MotionExtension] Extension config: {}".format(config))
-            self.config["camera"] = (
-                config.get("camera", self.config["camera"]) or self.config["camera"]
-            )
+            print("[MotionKinematicsExtension] Extension config: {}".format(config))
             self.config["effector"] = (
                 config.get("effector", self.config["effector"])
                 or self.config["effector"]
@@ -44,8 +40,8 @@ class MotionExtension(omni.ext.IExt):
                 config.get("server", self.config["server"]) or self.config["server"]
             )
         except Exception as e:
-            print("[MotionExtension] Extension config: {}".format(e))
-        print("[MotionExtension] Extension config: {}".format(self.config))
+            print("[MotionKinematicsExtension] Extension config: {}".format(e))
+        print("[MotionKinematicsExtension] Extension config: {}".format(self.config))
 
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
@@ -53,27 +49,17 @@ class MotionExtension(omni.ext.IExt):
         async def f(self):
             context = omni.usd.get_context()
             while context.get_stage() is None:
-                print("[MotionExtension] Extension world wait")
+                print("[MotionKinematicsExtension] Extension world wait")
                 await asyncio.sleep(0.5)
-            print("[MotionExtension] Extension world ready")
+            print("[MotionKinematicsExtension] Extension world ready")
 
             stage = context.get_stage()
-            print("[MotionExtension] Extension stage {}".format(stage))
-
-            if (
-                self.config["camera"]
-                and stage.GetPrimAtPath(self.config["camera"]).IsValid()
-            ):
-                self.camera = Camera(prim_path=self.config["camera"])
-                self.camera.initialize()
-                print("[MotionExtension] Extension camera {}".format(self.camera))
-            else:
-                self.camera = None
+            print("[MotionKinematicsExtension] Extension stage {}".format(stage))
 
             if self.config["articulation"]:
                 self.articulation = Articulation(self.config["articulation"])
                 print(
-                    "[MotionExtension] Extension articulation {} ({})".format(
+                    "[MotionKinematicsExtension] Extension articulation {} ({})".format(
                         self.articulation, self.articulation.dof_names
                     )
                 )
@@ -90,7 +76,7 @@ class MotionExtension(omni.ext.IExt):
                                         ws.recv(), timeout=1.0
                                     )
                                     print(
-                                        "[MotionExtension] Extension server: {}".format(
+                                        "[MotionKinematicsExtension] Extension server: {}".format(
                                             response
                                         )
                                     )
@@ -107,7 +93,7 @@ class MotionExtension(omni.ext.IExt):
 
                                         data = json.loads(body)
                                         print(
-                                            "[MotionExtension] Extension server: {}".format(
+                                            "[MotionKinematicsExtension] Extension server: {}".format(
                                                 data
                                             )
                                         )
@@ -123,7 +109,7 @@ class MotionExtension(omni.ext.IExt):
                                             )
                                         )
                                         print(
-                                            "[MotionExtension] Extension position: {}".format(
+                                            "[MotionKinematicsExtension] Extension position: {}".format(
                                                 self.position
                                             )
                                         )
@@ -133,46 +119,12 @@ class MotionExtension(omni.ext.IExt):
                     except asyncio.CancelledError:
                         raise
                     except Exception as e:
-                        print("[MotionExtension] Extension server: {}".format(e))
+                        print("[MotionKinematicsExtension] Extension server: {}".format(e))
                         await asyncio.sleep(1)
             except asyncio.CancelledError:
-                print("[MotionExtension] Extension server cancel")
+                print("[MotionKinematicsExtension] Extension server cancel")
             finally:
-                print("[MotionExtension] Extension server exit")
-
-        async def v(self):
-            try:
-                while self.running:
-                    try:
-                        image = self.camera.get_rgba()
-                        if len(image):
-                            image = np.array(image, dtype=np.uint8)  # RGBA
-                            assert image.shape[-1] == 4, "camera {}".format(image.shape)
-                            image = image[:, :, :3]  # Remove alpha channel, keep RGB
-                            assert image.shape[-1] == 3, "camera {}".format(image.shape)
-
-                            # buffer = io.BytesIO()
-                            # PIL.Image.fromarray(image, mode="RGB").save(
-                            #     buffer, format="JPEG2000", quality_mode="lossless"
-                            # )
-                            # self.socket.sendto(buffer.getvalue(), ("127.0.0.1", 6000))
-
-                            self.socket.sendto(image.tobytes(), ("127.0.0.1", 6000))
-
-                            # width, height = 128, 128
-                            # header = f"P6\n{width} {height}\n255\n".encode()
-                            # self.socket.sendto(
-                            #     header + image.tobytes(), ("127.0.0.1", 6000)
-                            # )
-                    except asyncio.CancelledError:
-                        raise
-                    except Exception as e:
-                        print("[MotionExtension] Extension camera: {}".format(e))
-                    await asyncio.sleep(1 / 30)  # Maintain ~30 FPS
-            except asyncio.CancelledError:
-                print("[MotionExtension] Extension camera cancel")
-            finally:
-                print("[MotionExtension] Extension camera exit")
+                print("[MotionKinematicsExtension] Extension server exit")
 
         self.position = None
         self.step_position = None
@@ -181,8 +133,7 @@ class MotionExtension(omni.ext.IExt):
         loop = asyncio.get_event_loop()
         loop.run_until_complete(f(self))
         self.server_task = loop.create_task(g(self))
-        self.camera_task = loop.create_task(v(self)) if self.camera else None
-        print("[MotionExtension] Extension startup")
+        print("[MotionKinematicsExtension] Extension startup")
 
         # self.subscription = (
         #    omni.kit.app.get_app()
@@ -191,7 +142,7 @@ class MotionExtension(omni.ext.IExt):
         # )
 
     def step(self, delta: float):
-        print("[MotionExtension] Extension step {}".format(delta))
+        print("[MotionKinematicsExtension] Extension step {}".format(delta))
 
         if self.position is not None:
             value = self.position
@@ -201,7 +152,7 @@ class MotionExtension(omni.ext.IExt):
                     R.from_quat(value[3:]) * R.from_quat(value[3:]).inv()
                 ).as_quat()
                 print(
-                    "[MotionExtension] Extension step {} {} {}".format(
+                    "[MotionKinematicsExtension] Extension step {} {} {}".format(
                         delta_p, delta_r, delta
                     )
                 )
@@ -214,22 +165,11 @@ class MotionExtension(omni.ext.IExt):
                 try:
                     await self.server_task
                 except asyncio.CancelledError:
-                    print("[MotionExtension] Extension server cancel")
+                    print("[MotionKinematicsExtension] Extension server cancel")
                 except Exception as e:
-                    print("[MotionExtension] Extension server exception {}".format(e))
-
-        async def v(self):
-            if getattr(self, "camera_task") and self.camera_task:
-                self.camera_task.cancel()
-                try:
-                    await self.camera_task
-                except asyncio.CancelledError:
-                    print("[MotionExtension] Extension camera cancel")
-                except Exception as e:
-                    print("[MotionExtension] Extension camera exception {}".format(e))
+                    print("[MotionKinematicsExtension] Extension server exception {}".format(e))
 
         self.running = False
         loop = asyncio.get_event_loop()
         loop.run_until_complete(g(self))
-        loop.run_until_complete(v(self))
-        print("[MotionExtension] Extension shutdown")
+        print("[MotionKinematicsExtension] Extension shutdown")
